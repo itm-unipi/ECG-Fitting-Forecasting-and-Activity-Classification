9e4c199b8a33d22a0f93e366592b0691cdd4c030
yolly98 <gianlucagemini98@gmail.com>
2023-09-27 17:12:13 +0200

    Added neural network fitting main
    Co-authored-by: Matteo Abaterusso <49238869+MatteoAba@users.noreply.github.com>
    Co-authored-by: Biagio Cornacchia <49314349+biagiocornacchia@users.noreply.github.com>

--- src/neural_networks_ecg_fitting/main.m
+++ src/neural_networks_ecg_fitting/main.m
@@ -4,8 +4,16 @@ clc;
 
 %% Constants
 
+% MLP constants
 MLP_MEAN_HIDDEN_LAYER_SIZE = 20;
 MLP_STD_HIDDEN_LAYER_SIZE = 20;
+
+% RBF Costants
+MAX_NEURONS_NUMBER = 100;
+NEURONS_NUMBER_STEP = 5;
+SPREAD_MEAN = 0.8;
+SPREAD_STD = 0.8;
+ERROR_GOAL = 0;
 
 addpath('./data_preprocessing');
 
@@ -21,16 +29,16 @@ t_std = final_ecg_std_targets_vector';
 %% MLP Mean Training and Test
 
 % Create and train a mlp network
-net_mean = fitnet(MLP_MEAN_HIDDEN_LAYER_SIZE, 'trainbr');
-net_mean.trainParam.showWindow = 0;
-[net_mean, tr] = train(net_mean, x_mean, t_mean);
+net = fitnet(MLP_MEAN_HIDDEN_LAYER_SIZE, 'trainbr');
+net.trainParam.showWindow = 0;
+[net, ~] = train(net, x_mean, t_mean);
 
 % Test the network and save results
-y_mean = net_mean(x_mean);
+y = net(x_mean);
 figure_id = 1;
-figure(figure_id), plotregression(t_mean, y_mean);
-mse_value = mse(y_mean', t_mean');
-regression_stats = fitlm(t_mean', y_mean');
+figure(figure_id), plotregression(y, t_mean);
+mse_value = mse(y', t_mean');
+regression_stats = fitlm(y', t_mean');
 r_value = sqrt(regression_stats.Rsquared.Ordinary);
 
 fprintf("MLP MEAN RESULTS: [mse: %d, r-value: %d]\n", mse_value, r_value);
@@ -39,19 +47,68 @@ saveas(figure_id, fullfile('../tmp', 'mlp_mean_result.png'));
 
 %% MLP Std Training and Test
 
+clear net;
+clear y;
+
 % Create and train a mlp network
-net_std = fitnet(MLP_MEAN_HIDDEN_LAYER_SIZE, 'trainbr');
-net_std.trainParam.showWindow = 0;
-[net_std, tr] = train(net_std, x_std, t_std);
+net = fitnet(MLP_STD_HIDDEN_LAYER_SIZE, 'trainbr');
+net.trainParam.showWindow = 0;
+[net, ~] = train(net, x_std, t_std);
 
 % Test the network and save results
-y_std = net_std(x_std);
+y = net(x_std);
 figure_id = figure_id + 1;
-figure(figure_id), plotregression(t_std, y_std);
-mse_value = mse(y_std', t_std');
-regression_stats = fitlm(t_std', y_std');
+figure(figure_id), plotregression(y, t_std);
+mse_value = mse(y', t_std');
+regression_stats = fitlm(y', t_std');
 r_value = sqrt(regression_stats.Rsquared.Ordinary);
 
-fprintf("MLP MEAN RESULTS: [mse: %d, r-value: %d]\n", mse_value, r_value);
+fprintf("MLP STD RESULTS: [mse: %d, r-value: %d]\n", mse_value, r_value);
 
 saveas(figure_id, fullfile('../tmp', 'mlp_std_result.png'));
+
+%% RBF Mean Training and test
+
+clear net;
+clear y;
+
+% Create and train a rbf network with Bayesian regularization
+net = newrb(x_mean, t_mean, ERROR_GOAL, SPREAD_MEAN, MAX_NEURONS_NUMBER, NEURONS_NUMBER_STEP);
+net.trainFcn = 'trainbr';
+net.trainParam.showWindow = 0;
+net = train(net, x_mean, t_mean);
+
+% Test the network and save results
+y = net(x_mean);
+figure_id = figure_id + 1;
+figure(figure_id), plotregression(y, t_mean);
+mse_value = mse(y', t_mean');
+regression_stats = fitlm(y', t_mean');
+r_value = sqrt(regression_stats.Rsquared.Ordinary);
+
+fprintf("RBF MEAN RESULTS: [mse: %d, r-value: %d]\n", mse_value, r_value);
+
+saveas(figure_id, fullfile('../tmp', 'rbf_mean_result.png'));
+
+%% RBF Std Training and test
+
+clear net;
+clear y;
+
+% Create and train a rbf network with Bayesian regularization
+net = newrb(x_std, t_std, ERROR_GOAL, SPREAD_STD, MAX_NEURONS_NUMBER, NEURONS_NUMBER_STEP);
+net.trainFcn = 'trainbr';
+net.trainParam.showWindow = 0;
+net = train(net, x_std, t_std);
+
+% Test the network and save results
+y = net(x_std);
+figure_id = figure_id + 1;
+figure(figure_id), plotregression(y, t_std);
+mse_value = mse(y', t_std');
+regression_stats = fitlm(y', t_std');
+r_value = sqrt(regression_stats.Rsquared.Ordinary);
+
+fprintf("RBF STD RESULTS: [mse: %d, r-value: %d]\n", mse_value, r_value);
+
+saveas(figure_id, fullfile('../tmp', 'rbf_std_result.png'));
