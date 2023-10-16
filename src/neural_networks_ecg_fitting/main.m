58be82dfd48cd57433806ba929f7b8ea29e21b7f
yolly98 <gianlucagemini98@gmail.com>
2023-10-16 11:43:49 +0200

    Added complete regression plot to ecg fitting main
    Co-authored-by: Matteo Abaterusso <49238869+MatteoAba@users.noreply.github.com>
    Co-authored-by: Biagio Cornacchia <49314349+biagiocornacchia@users.noreply.github.com>

--- src/neural_networks_ecg_fitting/main.m
+++ src/neural_networks_ecg_fitting/main.m
@@ -5,17 +5,22 @@ clc;
 %% Constants
 
 % MLP constants
-MLP_MEAN_HIDDEN_LAYER_SIZE = 20;
-MLP_STD_HIDDEN_LAYER_SIZE = 20;
+MLP_MEAN_HIDDEN_LAYER_SIZE = 85;
+MLP_STD_HIDDEN_LAYER_SIZE = 55;
+MLP_MEAN_MAX_EPOCHS = 350;
+MLP_STD_MAX_EPOCHS = 500;
 
 % RBF Costants
 MAX_NEURONS_NUMBER = 100;
 NEURONS_NUMBER_STEP = 5;
 FRACTION_RBF_TEST_SET = 0.15;
-SPREAD_MEAN = 0.8;
-SPREAD_STD = 0.8;
+SPREAD_MEAN = 0.44;
+SPREAD_STD = 0.388;
 ERROR_GOAL = 0;
+RBF_MEAN_MAX_EPOCHS = 500;
+RBF_STD_MAX_EPOCHS = 350;
 
+figure_id = 0;
 addpath('./data_preprocessing');
 rng("default");
 
@@ -32,14 +37,21 @@ t_std = final_ecg_std_targets_vector';
 
 % Create and train a mlp network
 net = fitnet(MLP_MEAN_HIDDEN_LAYER_SIZE, 'trainbr');
+net.trainParam.epochs = MLP_MEAN_MAX_EPOCHS;
+net.divideParam.trainRatio = 0.85;
+net.divideParam.testRatio = 0.15;
+net.divideParam.valRatio = 0;
 [net, tr] = train(net, x_mean, t_mean);
 
 % Test the network and save results
+train_x_mean = x_mean(:, tr.trainInd);
+train_t_mean = t_mean(:, tr.trainInd);
 test_x_mean = x_mean(:, tr.testInd);
 test_t_mean = t_mean(:, tr.testInd);
+train_y_mean = net(train_x_mean);
 test_y_mean = net(test_x_mean);
-figure_id = 1;
-figure(figure_id), plotregression(test_t_mean, test_y_mean);
+figure_id = figure_id + 1;
+figure(figure_id), plotregression(train_t_mean, train_y_mean, 'Training', test_t_mean, test_y_mean, 'Test');
 mse_value = mse(test_t_mean', test_y_mean');
 regression_stats = fitlm(test_t_mean', test_y_mean');
 r_value = sqrt(regression_stats.Rsquared.Ordinary);
@@ -55,14 +67,21 @@ clear y;
 
 % Create and train a mlp network
 net = fitnet(MLP_STD_HIDDEN_LAYER_SIZE, 'trainbr');
+net.trainParam.epochs = MLP_STD_MAX_EPOCHS;
+net.divideParam.trainRatio = 0.85;
+net.divideParam.testRatio = 0.15;
+net.divideParam.valRatio = 0;
 [net, tr] = train(net, x_std, t_std);
 
 % Test the network and save results
+train_x_std = x_std(:, tr.trainInd);
+train_t_std = t_std(:, tr.trainInd);
 test_x_std = x_std(:, tr.testInd);
 test_t_std = t_std(:, tr.testInd);
+train_y_std = net(train_x_std);
 test_y_std = net(test_x_std);
 figure_id = figure_id + 1;
-figure(figure_id), plotregression(test_t_std, test_y_std);
+figure(figure_id), plotregression(train_t_std, train_y_std, 'Training', test_t_std, test_y_std, 'Test');
 mse_value = mse(test_t_std', test_y_std');
 regression_stats = fitlm(test_t_std', test_y_std');
 r_value = sqrt(regression_stats.Rsquared.Ordinary);
@@ -73,28 +92,28 @@ saveas(figure_id, fullfile('../tmp', 'mlp_std_result.png'));
 
 %% Dataset partitioning for RBF
 
-clear training_x_mean;
-clear training_t_mean;
+clear train_x_mean;
+clear train_t_mean;
 clear test_x_mean;
 clear test_t_mean;
-clear training_x_std;
-clear training_t_std;
+clear train_x_std;
+clear train_t_std;
 clear test_x_std;
 clear test_t_std;
 
 partition_data = cvpartition(size(x_mean, 2), "Holdout", FRACTION_RBF_TEST_SET);
 
 % Mean dataset
-training_x_mean = x_mean(:, training(partition_data));
-training_t_mean = t_mean(:, training(partition_data));
+train_x_mean = x_mean(:, training(partition_data));
+train_t_mean = t_mean(:, training(partition_data));
 test_x_mean = x_mean(:, test(partition_data));
 test_t_mean = t_mean(:, test(partition_data));
 
 partition_data = cvpartition(size(x_std, 2), "Holdout", FRACTION_RBF_TEST_SET);
 
 % Std dataset
-training_x_std = x_std(:, training(partition_data));
-training_t_std = t_std(:, training(partition_data));
+train_x_std = x_std(:, training(partition_data));
+train_t_std = t_std(:, training(partition_data));
 test_x_std = x_std(:, test(partition_data));
 test_t_std = t_std(:, test(partition_data));
 
@@ -103,14 +122,16 @@ test_t_std = t_std(:, test(partition_data));
 clear net;
 
 % Create and train a rbf network with Bayesian regularization
-net = newrb(training_x_mean, training_t_mean, ERROR_GOAL, SPREAD_MEAN, MAX_NEURONS_NUMBER, NEURONS_NUMBER_STEP);
+net = newrb(train_x_mean, train_t_mean, ERROR_GOAL, SPREAD_MEAN, MAX_NEURONS_NUMBER, NEURONS_NUMBER_STEP);
 net.trainFcn = 'trainbr';
-net = train(net, training_x_mean, training_t_mean);
+net.trainParam.epochs = RBF_MEAN_MAX_EPOCHS;
+net = train(net, train_x_mean, train_t_mean);
 
 % Test the network and save results
+train_y_mean = net(train_x_mean);
 test_y_mean = net(test_x_mean);
 figure_id = figure_id + 1;
-figure(figure_id), plotregression(test_t_mean, test_y_mean);
+figure(figure_id), plotregression(train_t_mean, train_y_mean, 'Training', test_t_mean, test_y_mean, 'Test');
 mse_value = mse(test_t_mean', test_y_mean');
 regression_stats = fitlm(test_t_mean', test_y_mean');
 r_value = sqrt(regression_stats.Rsquared.Ordinary);
@@ -124,14 +145,16 @@ saveas(figure_id, fullfile('../tmp', 'rbf_mean_result.png'));
 clear net;
 
 % Create and train a rbf network with Bayesian regularization
-net = newrb(training_x_std, training_t_std, ERROR_GOAL, SPREAD_STD, MAX_NEURONS_NUMBER, NEURONS_NUMBER_STEP);
+net = newrb(train_x_std, train_t_std, ERROR_GOAL, SPREAD_STD, MAX_NEURONS_NUMBER, NEURONS_NUMBER_STEP);
 net.trainFcn = 'trainbr';
-net = train(net, training_x_std, training_t_std);
+net.trainParam.epochs = RBF_STD_MAX_EPOCHS;
+net = train(net, train_x_std, train_t_std);
 
 % Test the network and save results
+train_y_std = net(train_x_std);
 test_y_std = net(test_x_std);
 figure_id = figure_id + 1;
-figure(figure_id), plotregression(test_t_std, test_y_std);
+figure(figure_id), plotregression(train_t_std, train_y_std, 'Training', test_t_std, test_y_std, 'Test');
 mse_value = mse(test_t_std', test_y_std');
 regression_stats = fitlm(test_t_std', test_y_std');
 r_value = sqrt(regression_stats.Rsquared.Ordinary);
